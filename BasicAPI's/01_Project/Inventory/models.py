9983085873c0b2a134ce0daaf6455344a92e0c62
rsivaraman18 <126255552+rsivaraman18@users.noreply.github.com>
2024-07-31 15:06:52 +0530

    Update models.py

--- BasicAPI's/01_Project/Inventory/models.py
+++ BasicAPI's/01_Project/Inventory/models.py
@@ -5,5 +5,5 @@ from django.db import models
 class Myproducts(models.Model):
     product_name = models.CharField(max_length=200,null=True)
     code         = models.CharField(max_length=200,null=True)
-    price        = models.FloatField(default=0)
+    price        = models.FloatField(default=0) 
    
