82f94e8b876455053327b20d90d3555751aafda8
rsivaraman18 <126255552+rsivaraman18@users.noreply.github.com>
2024-08-07 16:55:03 +0530

    Update urls.py

--- BasicAPI's/01_Project/Inventory/urls.py
+++ BasicAPI's/01_Project/Inventory/urls.py
@@ -2,7 +2,9 @@
    
 from django.urls import path
 from .views import * 
-   
+
+
+
 urlpatterns = [
     path('products/',          Ourproducts.as_view() ),
     path('products/<int:id>/', Ourproductsbyid.as_view() ),
